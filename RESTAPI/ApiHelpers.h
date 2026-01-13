54495130e98f75721745bdc6a246334d1f34bed4
joetom <joetom@qubic.li>
2026-01-13 16:03:15 +0000

    build fix

--- RESTAPI/ApiHelpers.h
+++ RESTAPI/ApiHelpers.h
@@ -60,6 +60,7 @@ struct EpochInfo {
     int64_t endTickStartLogId = -1;
     int64_t endTickEndLogId = -1;
     uint32_t currentTick = 0;
+    uint32_t lastIndexedTick = 0;
     bool found = false;
     std::string error;
 };
